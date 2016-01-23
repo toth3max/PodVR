3523636c1eea869b8e1ac59b2d12b1daa6cba2a5
JAKOBSDATOR\Jakob <jakob.johanssonjj@gmail.com>
2016-01-23 17:02:36 +0100

    Added pizza hook and movement also added foodAttachement scripts

---
+++ Assets/Scripts/Archery/DropTrigger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DropTrigger : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<PizzaWheel>())
+        {
+            PizzaWheel pizzaWheel = other.GetComponent<PizzaWheel>();
+
+            pizzaWheel.DropPizza();
+
+        }
+    }
+}
---
+++ Assets/Scripts/Archery/PizzaManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PizzaManager : MonoBehaviour {
+
+    public GameObject pizzaHook;
+    public List<GameObject> allPizzaHooks;
+ 
+	// Use this for initialization
+	void Start () 
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+}
