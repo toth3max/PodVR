0b30883a5845091a8dcdb50ea4c89021648eae9c
SoftronicBona <bjorn.fyrvall@softronic.se>
2016-01-23 20:09:36 +0100

    sa

---
+++ Assets/Scripts/GameHub/SelectBox.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Collider))]
+public class SelectBox : MonoBehaviour
+{
+    public AudioSource AudioSource;
+    public AudioClip SelectSound;
+    public AudioClip AcceptSound;
+
+    [HideInInspector]
+    public string LevelToLoad;
+
+	void Start()
+    {
+        AudioSource = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        AudioSource.PlayOneShot(SelectSound);
+    }
+
+    void OnMouseEnter()
+    {
+        AudioSource.PlayOneShot(SelectSound);
+    }
+
+    void OnMouseDown()
+    {
+        LoadLevel();
+    }
+
+    public void LoadLevel()
+    {
+        AudioSource.PlayOneShot(AcceptSound);
+        //SceneManager.LoadScene(LevelToLoad);
+    }
+}
