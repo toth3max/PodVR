a2dbe4d287d3aa4db4f36b23f0eeea55f7c570cb
SoftronicBona <bjorn.fyrvall@softronic.se>
2016-01-23 17:02:34 +0100

    ASD

---
+++ Assets/Scripts/FollowMe/Pointer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pointer : MonoBehaviour
+{
+    public Transform Target;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Target != null) {
+            transform.position = Target.position;
+            transform.rotation = Target.rotation;
+        }
+	}
+}
