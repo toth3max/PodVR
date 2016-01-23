d130587fe59ac4b4764f1667c403d8a8ac50b873
Dee-Kej <daniel_kk9012@hotmail.com>
2016-01-23 13:32:13 +0100

    trying to add touchpad input

---
+++ Assets/Scripts/DK/TouchPadInput.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using Valve.VR;
+
+public class TouchPadInput : MonoBehaviour {
+
+    private float horizontalTouchpad;
+    private float verticalTouchpad;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var state = new VRControllerState_t();
+
+        horizontalTouchpad = state.rAxis[(int)EVRButtonId.k_EButton_SteamVR_Touchpad].x;
+        verticalTouchpad = state.rAxis[(int)EVRButtonId.k_EButton_SteamVR_Touchpad].y;
+
+        Debug.Log(horizontalTouchpad);
+    }
+}
