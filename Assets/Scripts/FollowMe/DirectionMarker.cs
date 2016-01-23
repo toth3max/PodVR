677667f767aaf65d11b6bc7f9b49bd6739b85ecd
SoftronicBona <bjorn.fyrvall@softronic.se>
2016-01-23 13:51:16 +0100

    Added Follow me Scene.

---
+++ Assets/Scripts/FollowMe/DirectionMarker.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SteamVR_TrackedObject))]
+public class DirectionMarker : MonoBehaviour
+{
+    public LineRenderer LineRenderer;
+    private Ray ray;
+    private RaycastHit hit;
+
+    private SteamVR_TrackedObject trackedObj;
+    private SteamVR_Controller.Device device;
+
+    void Awake()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    void AquireDevice()
+    {
+        device = SteamVR_Controller.Input((int)trackedObj.index);
+    }
+
+    void FixedUpdate()
+    {
+        if (device == null) {
+            AquireDevice();
+        }
+
+        if (device.GetTouch(SteamVR_Controller.ButtonMask.Trigger)) {
+            LineRenderer.enabled = true;
+        } else {
+            LineRenderer.enabled = false;
+        }
+    }
+   
+    public void UseHaptics(uint force)
+    {
+        ushort hapticForce = (ushort)force;
+        SteamVR_Controller.Input((int)trackedObj.index).TriggerHapticPulse(hapticForce);
+    }
+}
