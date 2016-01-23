3fec949c1a9b7166bab6c6587dac65edd7f4578c
SoftronicBona <bjorn.fyrvall@softronic.se>
2016-01-23 21:49:40 +0100

    asasa

--- Assets/Scripts/FollowMe/Target.cs
+++ Assets/Scripts/FollowMe/Target.cs
@@ -1,28 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class Target : MonoBehaviour
 {
     public List<Follower> FollowersGathered;
+    public Text Text;
 
 	// Use this for initialization
 	void Start () {
         FollowersGathered = new List<Follower>();
+        Text = GetComponent<Text>();
 	}
 	
     public void AddFollower(Follower follower)
     {
         FollowersGathered.Add(follower);
-        Debug.Log("Gathered:" + FollowersGathered.Count);
+        UpdateText();
     }
 
     public void RemoveFollower(Follower follower)
     {
         FollowersGathered.Remove(follower);
-        Debug.Log("Gathered:" + FollowersGathered.Count);
 
     }
+
+    public void UpdateText()
+    {
+        Text.text = string.Format("{0}/{10}", FollowersGathered.Count, FollowerManager.Instance.SpawnAmount);
+        UpdateText();
+    }
+
 	// Update is called once per frame
 	void Update () {
 	
