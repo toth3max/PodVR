264c4e18b97ff999ec2b6de1989b2cbe30fc116e
Dee-Kej <daniel_kk9012@hotmail.com>
2016-01-23 04:21:27 +0100

    more details to PodStorage

---
+++ Assets/Scripts/DK/SirenSpin.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SirenSpin : MonoBehaviour {
+    public float sirenSpeed = 3f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Rotate(0, 0, sirenSpeed);
+	}
+}
