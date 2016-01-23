28aae05b7d34cea447dbb0f00642ec0eb90b3ea4
binarylegend <daniel.nyberg@vobling.com>
2016-01-23 13:39:53 +0100

    Updated SuperPose gameplay

---
+++ Assets/Scripts/SuperPoser/PoseManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PoseManager : MonoBehaviour {
+
+    public bool LeftTriggered, RightTriggered, HeadTriggered;
+    public GameObject[] SuperPoses;
+    private int numPoses;
+    private int poseIndex;
+
+	// Use this for initialization
+	void Start () {
+        LeftTriggered = RightTriggered = HeadTriggered = false;
+        numPoses = SuperPoses.Length;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(LeftTriggered && RightTriggered && HeadTriggered)
+        {
+            //Pose completed
+            poseIndex++;
+            //Go to next pose
+            //Or if all poses done, win
+            if(poseIndex < numPoses)
+            {
+                //We won
+            }
+        }
+	}
+}
